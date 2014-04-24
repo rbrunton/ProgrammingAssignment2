c3cc8c2198a95d61d6aeeb71d6bf612c7cdc43a8
rbrunton <ryan.brunton@gmail.com>
2014-04-24 10:50:45 -0400

    Function stubs implemented in compliance with the instructions for programming assignment two.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,48 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The two functions in this file support caching the results of calculating the inverse of a matrix.
+#  The "cacheSolve" method takes a single argument, an object created by the "makeCacheMatrix"
+#  function, and returns the inverse of the R matrix passed to the "makeCacheMatrix" function
+#  to create the object.
 
-## Write a short comment describing this function
-
+## This function creates an object which wraps an R matrix and provides accessor and setter
+#  functions that utilize the R scoping rules to return a cached copy of the inverse of the
+#  wrapped matrix if that inverse was previously calculated. The "x" argument is a square matrix.
 makeCacheMatrix <- function(x = matrix()) {
-
+      #start by setting cached inverse to NULL
+      inverse <- NULL
+      #allow the wrapped matrix to be modified; if it is modified, reset inverse to NULL
+      set <- function(y) {
+        x <<- y
+        inverse <<- NULL
+      }
+      #accessor method for the wrapped matrix
+      get <- function() x
+      #function to set the inverse of the wrapped matrix
+      setinverse <- function(inverse) inverse <<- inverse
+      #accessor function to return the inverse
+      getinverse <- function() inverse
+      #return the list of functions, indexed by function name
+      list(set = set, get = get,
+           setinverse = setinverse,
+           getinverse = getinverse)
 }
 
-
-## Write a short comment describing this function
-
+## This function solves for the inverse of a matrix or retrieves the previously calculated
+#  inverse of the matrix. The "x" argument variable is an object created using by calling the 
+#  "makeCacheMatrix" function on a square R matrix.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+      #get the inverse variable from the scope of "x"
+      inverse <- x$getinverse()
+      #if retrieved value is not NULL, then the inverse was previously calculated, so return
+      #the cached value
+      if(!is.null(inverse)) {
+        return(inverse)
+      }
+      #...otherwise get the wrapped square matrix
+      data <- x$get()
+      #...and solve for the inverse
+      inverse <- solve(data)
+      #now set the inverse value in the scope of "x" for future cache retrieval
+      x$setinverse(inverse)
+      #return the inverse of the matrix wrapped by "x"
+      inverse
 }
